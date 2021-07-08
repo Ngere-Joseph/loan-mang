b3b250f587a2e8af8896d164e713e61391053969
joseph <joseph_ngere@protonmail.com>
2021-07-08 19:24:37 +0100

    initial commit

---
+++ SPApp3/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SPApp3.Data.Company;
+
+namespace SPApp3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ SPApp3/Data/Company/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPApp3.Data.Company
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Email { get; set; }
+    }
+}
